e809d9cb322bd8e46ca52d4828b1fceeb12a9964
saran kaarthik <55199503+sarankaarthik@users.noreply.github.com>
2021-03-28 12:56:21 +0530

    Create 9(usingmap).cpp

---
+++ Week1/9(usingmap).cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<map>
+
+using namespace std;
+
+void odd(int a[], int n)
+{
+    map<int, int> occ;
+    for(int i = 0; i < n; i++)
+    {
+        occ[a[i]]++;
+    }
+    for(int i = 0; i < n; i++)
+    {
+        if(occ[a[i]] % 2 != 0)
+        {
+            cout<<"\n The number which occurs odd number of times is : "<<a[i];
+        }
+    }
+
+}
+
+int main()
+{
+    int a[] = {1, 2, 3, 2, 1, 2, 3, 1};
+    int n = sizeof(a)/sizeof(a[0]);
+    odd(a,n);
+}
