8acad313c51346efce1b16b6d735df3610a9832b
saran kaarthik <55199503+sarankaarthik@users.noreply.github.com>
2021-03-30 21:30:50 +0530

    Update 5.cpp

--- Week1/5.cpp
+++ Week1/5.cpp
@@ -4,19 +4,19 @@
 
 using namespace std;
 
-void miss(vector<int> a, int n)
+void missing(vector<int> a, int n)
 {
-    map<int, int> occ;
+    map<int, int> miss;
     for(int i = 0; i < n; i++)
     {
         if(a[i] > 0)
         {
-            occ[a[i]]++;
+            miss[a[i]]++;
         }
     }
     for(int i = 1; i <= n+1; i++)
     {
-        if(occ[i] == 0)
+        if(miss[i] == 0)
         {
             cout<<i;
             return;
@@ -36,5 +36,5 @@ int main()
         cin>>x;
         a.push_back(x);
     }
-    miss(a,n);
+    missing(a,n);
 }
