86669ed010f49412e34e57a3afc12017c37b8ad0
saran kaarthik <55199503+sarankaarthik@users.noreply.github.com>
2021-04-02 22:05:33 +0530

    Create comp(pairmethod).cpp

---
+++ Week1/comp(pairmethod).cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+
+using namespace std;
+
+struct Pair
+{
+    int min;
+    int max;
+};
+ 
+struct Pair getMinMax(int arr[], int n)
+{
+    struct Pair minmax;    
+    int i;
+    if (n == 1)
+    {
+        minmax.max = arr[0];
+        minmax.min = arr[0];    
+        return minmax;
+    }
+     
+    if (arr[0] > arr[1])
+    {
+        minmax.max = arr[0];
+        minmax.min = arr[1];
+    }
+    else
+    {
+        minmax.max = arr[1];
+        minmax.min = arr[0];
+    }
+     
+    for(i = 2; i < n; i++)
+    {
+        if (arr[i] > minmax.max)    
+            minmax.max = arr[i];
+             
+        else if (arr[i] < minmax.min)    
+            minmax.min = arr[i];
+    }
+    return minmax;
+}
+ 
+int main()
+{
+    int arr[] = {1, 15, 6, 12, 2};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    struct Pair minmax = getMinMax(arr, arr_size);
+    cout << "\n Minimum element is " << minmax.min;
+    cout << "\n Maximum element is " << minmax.max;
+    return 0;
+}
